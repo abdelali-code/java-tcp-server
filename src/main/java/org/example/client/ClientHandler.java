0807b445083c14de3eab163d997b03181292e49c
aelouadi <abdelalielouadi8@gmail.com>
2026-03-04 00:47:13 +0100

    addd basic tp functionality

--- src/main/java/org/example/client/ClientHandler.java
+++ src/main/java/org/example/client/ClientHandler.java
@@ -1,4 +1,38 @@
 package org.example.client;
 
-public class ClientHandler {
+import java.io.*;
+import java.net.Socket;
+import java.time.LocalDateTime;
+
+public class ClientHandler implements Runnable{
+    Socket socket;
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Client connecté : " + socket.getInetAddress().getHostAddress());
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            String msg = in.readLine();
+            System.out.println("recieved message is " + msg);
+            if ("hello".equals(msg)) {
+                out.println("Bonjour client !");
+            }else if ("time".equals(msg)) {
+                out.println("time is " + LocalDateTime.now().toString()) ;
+            } else if ("bye".equals(msg)) {
+                in.close();
+                out.close();
+                socket.close();
+            }
+            else {
+                out.println("Message reçu : " + msg);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/org/example/server/MultiThreadServer.java
+++ src/main/java/org/example/server/MultiThreadServer.java
@@ -1,4 +1,28 @@
 package org.example.server;
 
+import org.example.client.ClientHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MultiThreadServer {
+
+    static ExecutorService executorService = Executors.newFixedThreadPool(5);
+    static int port = 5000;
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("server started on port " + port);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                executorService.submit(new ClientHandler(socket));
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
